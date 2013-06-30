c426b8a72e8f9d6397ce03151f0eab52b93bb961
Cameron Bytheway <cameron@nujii.com>
2013-06-29 20:33:08 -0600

    Initial commit

---
+++ src/fast_key.erl
@@ -0,0 +1,13 @@
+-module(fast_key).
+
+-export([get/2]).
+-export([get/3]).
+
+get(Key, Proplist)->
+  get(Key, Proplist, undefined).
+
+get(Key, Proplist, Default)->
+  case lists:keyfind(Key, 1, Proplist) of
+    {_, undefined} -> Default;
+    {_, Value} -> Value
+  end.
